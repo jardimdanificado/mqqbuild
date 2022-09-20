091dbd12f2d263e8cbe78b3b6a3f8642c328cf5e
jardimdanificado <jardeltorreslima01@gmail.com>
2022-09-20 13:44:05 -0300

    fixes to termux sceipts

--- builder/termuxsetup.sh
+++ builder/termuxsetup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 pkg install build-essential clang emscripten git
-#rm -r raylib
-#git clone https://github.com/raysan5/raylib --depth 1
+rm -r raylib
+rm -r build
+rm -r lib
+mkdir build
+mkdir lib
+
+git clone https://github.com/raysan5/raylib --depth 1
 
 cd raylib/src
 make PLATFORM=PLATFORM_WEB || make PLATFORM=PLATFORM_WEB  -e
-mv libraylib.a ../../lib/$TARGET
+mv libraylib.a ../../lib/
 cp raylib.h ../../include
 make clean || make clean -e || rm -fv *.o
 cd ../..
